5bebc07eeb3889b45b97ae7176e86f119289d0ac
Wlad Kowalew <wladickowalew@gmail.com>
2020-02-02 15:11:51 +0300

    client complete

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 let express = require("express");
+let cors = require("cors");
 let app = express();
+
+
+app.use(cors())
 
 app.get("/", function (request, response) {
 	response.send("Hello, Node.js!");
